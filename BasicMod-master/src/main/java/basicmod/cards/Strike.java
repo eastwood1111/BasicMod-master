94822b2146753b8347bdf56a0dee4a82aad6ab54
eastwood1111 <joeeastwoodiiii1@gmail.com>
2025-10-14 17:26:54 +0800

    创建卡片

---
+++ BasicMod-master/src/main/java/basicmod/cards/Strike.java
@@ -0,0 +1,45 @@
+package basicmod.cards;
+
+import basicmod.charater.MyCharacter;
+import basicmod.util.CardStats;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Strike extends BaseCard {
+    public static final String ID = makeID(Strike.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.BASIC,
+            CardTarget.ENEMY,
+            1
+    );
+
+    private static final int DAMAGE = 6;
+    private static final int UPG_DAMAGE = 3;
+
+    public Strike() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+
+        tags.add(CardTags.STARTER_STRIKE);
+        tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+    }
+
+
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Strike();
+    }
+}
